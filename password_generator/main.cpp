af594b0ff3b16413d1d97a55e807696d250318f6
cendyz775 <arek53731@gmail.com>
2026-01-25 20:40:42 +0100

    selecting options

--- password_generator/main.cpp
+++ password_generator/main.cpp
@@ -1,44 +1,133 @@
 #include <algorithm>
+#include <ios>
 #include <iostream>
+#include <limits>
 #include <map>
 #include <string>
 #include <vector>
 using namespace std;
 
 void welcome();
-void showing_options(const map<int, string> &, vector<string> &);
+void showing_options(map<int, string> *, vector<string> &);
+void selecting_options();
+void adding_deleting_options(map<int, string> *, map<int, string> *,
+                             vector<string> *, vector<string> &, int);
+void continue_adding_options();
+template <typename T> void toggle_all(vector<T> &, T);
 
-const map<int, string> options_names{{1, "lowercase letters"},
-                                     {2, "uppercase letters"},
-                                     {3, "digits letters"},
-                                     {4, "special letters"}};
+map<int, string> *options_names{new map<int, string>};
+map<int, string> *options_values{new map<int, string>};
 
 vector<string> selected_options{};
+vector<string> selected_values{};
+string still_choosing{"y"};
 
 int main() {
+  (*options_names)[1] = "lowercase letters";
+  (*options_names)[2] = "uppercase letters";
+  (*options_names)[3] = "digits";
+  (*options_names)[4] = "special symbols";
+
+  (*options_values)[1] = "abcdefghijklmnopqrstuvwxyz";
+  (*options_values)[2] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  (*options_values)[3] = "0123456789";
+  (*options_values)[4] = "!@#$%^&*";
 
   welcome();
 
-  showing_options(options_names, selected_options);
+  while (selected_options.size() < 2 && still_choosing == "y") {
+    showing_options(options_names, selected_options);
+    selecting_options();
+  }
+
+  delete options_names;
+  delete options_values;
+  // delete selected_options;
 
   return 0;
 }
 
 void welcome() {
   cout << "Welcome to the password generator!" << '\n';
-  cout << "For first select minimum two password options.";
+  cout << "For first select minimum two password options.\n\n";
 }
 
-void showing_options(const map<int, string> &opts, vector<string> &user_opts) {
+void showing_options(map<int, string> *opts, vector<string> &user_opts) {
+  for (auto &[key, value] : *opts) {
+    cout << key << ". " << value << '\n';
+  }
+}
 
-  for (auto &[key, value] : opts) {
-    int j{1};
+void selecting_options() {
+  int num{};
+  char c;
+  string s;
 
-    if (find(user_opts.begin(), user_opts.end(), value) != user_opts.end()) {
-      cout << "\033[32m" << j << ". " << key << "\033[0m\n";
-    } else {
-      cout << j << ". " << key << '\n';
+  while (still_choosing == "y") {
+    cout << "\tWhat option do you want to add: ";
+
+    if (!(cin >> num) || (cin.get(c) && c != '\n') || (num > 4 && num < 1)) {
+      cout << "Wrong option. Try again: ";
+      cin.clear();
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');
+      continue;
     }
-    ++j;
+
+    cout << '\n';
+
+    cout << "Option added." << '\n';
+    cout << "Current options: ";
+
+    toggle_all(selected_options, (*options_names)[num]);
+    toggle_all(selected_values, (*options_values)[num]);
+
+    for (int i{}; i < selected_options.size(); ++i) {
+      if (i == selected_options.size() - 1) {
+        cout << "\033[32m" << (selected_options)[i] << "\033[0m";
+      } else {
+        cout << "\033[32m" << (selected_options)[i] << "\033[0m" << ", ";
+      }
+    }
+
+    cout << "\n\n";
+
+    if (selected_values.size() >= 2) {
+      cout << "There is already " << selected_values.size()
+           << " selected options." << '\n';
+      cout << "Do you want to continue adding options? Y/N: ";
+
+      continue_adding_options();
+    }
+  }
+}
+
+template <typename T> void toggle_all(vector<T> &v, T value) {
+  auto it = find(v.begin(), v.end(), value);
+
+  if (it != v.end()) {
+    v.erase(it);
+  } else {
+    v.push_back(value);
+  }
+}
+
+void continue_adding_options() {
+  string s;
+
+  while (true) {
+    getline(cin, s);
+
+    if (s.size() != 1) {
+      cout << "Wrong key. Try again: ";
+    }
+
+    if (s == "y" || s == "Y") {
+      return;
+    } else if (s == "n" || s == "N") {
+      still_choosing = "n";
+      return;
+    } else {
+      cout << "Wrong key. Try again: ";
+    }
   }
 }
