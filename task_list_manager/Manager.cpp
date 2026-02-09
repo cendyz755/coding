3ab872182ce437aff75b5ea85e0455a47a29f9b2
cendyz775 <arek53731@gmail.com>
2026-02-09 19:57:09 +0100

    delete task func

--- task_list_manager/Manager.cpp
+++ task_list_manager/Manager.cpp
@@ -20,11 +20,26 @@ Manager::Manager() {
     cout << "\033[94mtasks.txt file loaded sucessfully\033[0m" << '\n';
   }
 
+  this->tasks = this->taskCounter();
+
   int opts_size{static_cast<int>(this->options.size())};
 
   for (int i{}; i < opts_size; ++i) {
     cout << '\t' << i + 1 << ". " << this->options[i] << '\n';
   }
+}
+
+int Manager::taskCounter() {
+  std::ifstream file("tasks.txt");
+  int lineNum{};
+
+  string line;
+
+  while (getline(file, line)) {
+    ++lineNum;
+  }
+
+  return lineNum;
 }
 
 void Manager::selectingOption() {
@@ -47,6 +62,16 @@ void Manager::selectingOption() {
       this->checkTasks();
     } else if (unixZero == 2) {
       this->addTask();
+    } else if (unixZero == 3) {
+
+    } else if (unixZero == 4) {
+      this->deleteTask();
+    } else if (unixZero == 5) {
+      cout << "Bye." << '\n';
+      break;
+    } else {
+      cout << "Wrong command. Try again: ";
+      continue;
     }
 
     cout << "What do you want do now?: ";
@@ -78,4 +103,47 @@ void Manager::addTask() {
   getline(cin, line);
 
   file << line << '\n';
+}
+
+void Manager::deleteTask() {
+  string line;
+  std::ifstream in_file("tasks.txt");
+  std::ofstream out_file("temp.txt");
+
+  cout << "Which task do you want to be deleted? (insert line number): ";
+
+  char line_to_delete;
+  int current_line_num{1};
+
+  while (true) {
+    getline(cin, line);
+
+    if (line.size() != 1) {
+      cout << "Wrong line. Try again: ";
+      continue;
+    }
+
+    line_to_delete = line[0] - this->unixCodeToZero;
+
+    if (line_to_delete < this->tasks || line_to_delete > this->tasks) {
+      cout << "Wrong line. Try again: ";
+      continue;
+    } else {
+      while (getline(in_file, line)) {
+        if (current_line_num != line_to_delete) {
+          out_file << line << '\n';
+        }
+        ++current_line_num;
+      }
+
+      in_file.close();
+      out_file.close();
+
+      fs::remove("tasks.txt");
+      fs::rename("temp.txt", "tasks.txt");
+
+      cout << "Task deleted." << '\n';
+      break;
+    }
+  }
 }
--- task_list_manager/Manager.h
+++ task_list_manager/Manager.h
@@ -10,10 +10,13 @@ private:
   vector<string> options{"Check current tasks", "Add task", "Edit task",
                          "Delete task", "Exit"};
   int unixCodeToZero{48};
+  int tasks{};
 
 public:
   Manager();
   void selectingOption();
   void checkTasks();
   void addTask();
+  void deleteTask();
+  int taskCounter();
 };
